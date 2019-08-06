571e91cde5ed6489ddcef167bc908a641a6c1e12
Marianne Pyon <mariannepyon@gmail.com>
2019-08-06 14:38:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,13 +2,8 @@
 def prime?(num)
   n = 2
   while n < num
-    is_divisible = ((num % i) == 0)
-
-    if is_divisible
-      return false
+    return false if num % n == 0
+    n += 1
   end
-    i += 1
-  end
-  
   true
 end
